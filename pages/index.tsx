06ce09eb8ae3774dc664e13823ed8fe352c4e6cc
Serhiy Hvala <serhiyhvaladew@gmail.com>
2023-02-26 21:26:30 +0200

    Create Hero Section

--- pages/index.tsx
+++ pages/index.tsx
@@ -13,9 +13,23 @@ const Home: NextPage = () => {
         <div className="min-h-screen bg-white">
           <div className="py-24 sm:py-32 lg:py-40">
             <div className="mx-auto max-w-7xl px-2 lg:px-4">
+              <div className="sm:text-center">
+                <h2 className='text-lg font-semibold leading-8 text-indigo-500'>
+                  Ovulation Calculator
+                </h2>
+                <p className='mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl'>
+                  Determine your ovulation cycle.
+                </p>
+                <p className='mx-auto mt-6 max-w-2xl text-lg leading-8 text-gray-600'>
+                  Use this calculator to pinpoint your most fertile days by identifying when you are likely ovulating.
+                  Menstrual periods can vary from person to person and month to month, so this tool can help you better
+                  understand your own cycle.
+                </p>
+              </div>
               <div className="mt-20 py-4 text-center">
                 <p className="text-gray-400">Powered by</p>
-                <a href="https://github.com/serhiyhvala" className='text-indigo-500 font-medium' target='_blank' rel='noreferrer'>Hvala Sergiy</a>
+                <a href="https://github.com/serhiyhvala" className='text-indigo-500 font-medium' target='_blank'
+                   rel='noreferrer'>Hvala Sergiy</a>
               </div>
             </div>
           </div>
@@ -25,3 +39,4 @@ const Home: NextPage = () => {
 }
 
 export default Home
+
