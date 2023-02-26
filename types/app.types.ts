84fd505603b1e13515df879c928c02c19ccf5d2b
Serhiy Hvala <serhiyhvaladew@gmail.com>
2023-02-26 23:20:50 +0200

    Update Types For Project

---
+++ types/app.types.ts
@@ -0,0 +1,38 @@
+import {ChangeEvent} from "react";
+
+export interface HeadlineProps {
+  primary: string,
+  secondary: string
+}
+
+export interface InputProps {
+  value?: string
+  name: string;
+  onChange: (e: ChangeEvent<HTMLInputElement>) => void;
+}
+
+export interface CycleLengthProps {
+  onQuickSelection: (value: number) => void;
+}
+
+export interface TagProps extends CycleLengthProps {
+  value: number;
+}
+
+export interface FormState {
+  lastPeriod: string | Date | null;
+  cycleLength: number | null;
+}
+
+export interface OutcomesState {
+  fertileWindow: Date[];
+  ovulationDate: Date | null;
+  nextPeriod: Date | null;
+  pregnancyTestDay: Date | null;
+  expectedDueDate: Date | null;
+}
+
+export interface Action {
+  type: string;
+  payload?: any;
+}
