74d67574a6a4a76db6d803bc954d5b97f10e6896
Serhiy Hvala <serhiyhvaladew@gmail.com>
2023-02-27 12:07:11 +0200

    Create Function For Calculate a Period Dates

---
+++ utils/index.ts
@@ -0,0 +1,49 @@
+import {addDays, addWeeks, format, isValid} from 'date-fns';
+import {DATE_FORMAT} from '@/constants';
+
+const calculateFertileWindow = (ovulationDate:number | Date) => {
+	const fertileWindowStart = addDays(ovulationDate, -5)
+	const fertileWindowEnd = addDays(ovulationDate, 1)
+	return [fertileWindowStart, fertileWindowEnd]
+};
+
+const calculateOvulationDate = (lastPeriod:number | Date, cycleLength:number) => {
+	return addDays(lastPeriod, cycleLength - 14)
+};
+const calculateNextPeriod = (lastPeriod:number | Date, cycleLength:number) => {
+	return addDays(lastPeriod, cycleLength)
+};
+
+const calculatePregnancyTestDay = (lastPeriod:number | Date) => {
+	return addWeeks(lastPeriod, 4)
+};
+
+const calculateExpectedDueDate = (lastPeriod:number | Date) => {
+	return addDays(lastPeriod, 280)
+};
+
+const toNumber = (value: string) => {
+	return isNaN(parseInt(value, 10)) ? '' : parseInt(value, 10);
+};
+
+function isNotEmpty(value: string) {
+	return value !== undefined && value !== null && value !== '';
+}
+
+function toFormatted(date: Date | number, formatString = DATE_FORMAT) {
+	if (!isValid(date)) {
+		return null;
+	}
+	return format(date, formatString);
+}
+
+function calculateOutcomes(lastPeriod:number | Date, cycleLength:number) {
+	const ovulationDate = calculateOvulationDate(lastPeriod, cycleLength)
+	const fertileWindow = calculateFertileWindow(ovulationDate)
+	const nextPeriod = calculateNextPeriod(lastPeriod, cycleLength)
+	const pregnancyTestDay = calculatePregnancyTestDay(lastPeriod)
+	const expectedDueDate = calculateExpectedDueDate(lastPeriod)
+	return {ovulationDate, fertileWindow, nextPeriod, pregnancyTestDay, expectedDueDate}
+}
+
+export {toNumber, isNotEmpty, toFormatted, calculateOutcomes};
