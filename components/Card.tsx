20c8747975dd838b2388c22e5a7686c7c1e9c492
Serhiy Hvala <serhiyhvaladew@gmail.com>
2023-02-27 12:06:04 +0200

    Create Card Component

---
+++ components/Card.tsx
@@ -0,0 +1,15 @@
+import {CardProps} from "@/types/app.types";
+
+const Card = ({date, title, description}: CardProps) => {
+  return (
+      <div className='bg-slate-900 rounded-lg px-6 py-8 shadow-xl'>
+        <div>
+          <span className='text-white inline-flex items-center justify-center p-2 bg-indigo-500 rounded-md shadow-lg'>{date}</span>
+        </div>
+        <h3 className='text-white mt-5 text-base font-medium tracking-tight'>{title}</h3>
+        <p className='text-slate-400 mt-2 text-sm'>{description}</p>
+      </div>
+  );
+};
+
+export default Card;
