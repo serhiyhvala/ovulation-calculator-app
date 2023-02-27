1ea0eb1c8a469fc50bc2340cf849de478535a128
Serhiy Hvala <serhiyhvaladew@gmail.com>
2023-02-27 14:03:20 +0200

    Update Input

--- components/Input.tsx
+++ components/Input.tsx
@@ -1,9 +1,10 @@
 import {InputProps} from "@/types/app.types";
 
 const Input = ({name, onChange, value}: InputProps) => {
+  const inputValue = !value ? '' : value
   return (
       <>
-        <input type="number" name={name} placeholder='ex. 28' onChange={onChange} value={value}
+        <input type="number" name={name} placeholder='ex. 28' onChange={onChange} value={inputValue}
                className='mt-1 block w-full rounded-md border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50'
         />
       </>
